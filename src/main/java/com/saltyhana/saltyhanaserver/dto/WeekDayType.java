85020f3ebefedacb762996f163f2fd4d364a7c1b
haneul0509 <haneul0509@naver.com>
2024-12-13 14:35:21 +0900

    :memo: WeekDayType 수정

--- src/main/java/com/saltyhana/saltyhanaserver/dto/WeekDayType.java
+++ src/main/java/com/saltyhana/saltyhanaserver/dto/WeekDayType.java
@@ -1,7 +1,12 @@
 package com.saltyhana.saltyhanaserver.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Data
+@Builder
 public class WeekDayType {
     private LocalDate date;
     private Boolean isAchieve;
