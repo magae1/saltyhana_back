5c04cf4b183f3daadb12e554f71945b49cbb1cde
commaengddongyeon <kimdongyn927@naver.com>
2024-12-13 13:26:51 +0900

    :necktie: 상품 추천 페이지 DTO

---
+++ src/main/java/com/saltyhana/saltyhanaserver/dto/RecommendResponseDTO.java
@@ -0,0 +1,12 @@
+package com.saltyhana.saltyhanaserver.dto;
+
+
+public class RecommendResponseDTO {
+    private int id;
+    private String title;
+    private String subtitle;
+    private String imageUrl;
+    private String description;
+
+    private String tendency;
+}
