981602aa0e92e5a4c3b64b8565a7f18eb129d789
commaengddongyeon <kimdongyn927@naver.com>
2024-12-13 13:53:06 +0900

    :necktie: RecommendResponseDTO

---
+++ src/main/java/com/saltyhana/saltyhanaserver/dto/RecommendResponseDTO.java
@@ -0,0 +1,12 @@
+package com.saltyhana.saltyhanaserver.dto;
+
+
+public class RecommendResponseDTO {
+    private int type;   // "asset" - 0 | "tendency" - 1 (자산 기반 추천인지 성향 테스트 기반 추천인지)
+    private String title;
+    private String subTitle;
+    private String imageUrl;    // 카드 안에 들어갈 로고 이미지
+    private String description;
+
+    private String tendency     // 성향 테스트 결과 (String | null)
+}
