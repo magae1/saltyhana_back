d9e66de6a1d4738424862fc5d0aa82ce0240854f
magae1 <jjilil8351@gmail.com>
2024-12-15 17:23:10 +0900

    ✅ jwt 테스트 코드 추가

---
+++ src/test/java/com/saltyhana/saltyhanaserver/provider/JWTProviderTest.java
@@ -0,0 +1,52 @@
+package com.saltyhana.saltyhanaserver.provider;
+
+
+import java.util.Date;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.saltyhana.saltyhanaserver.exception.InvalidJWTException;
+
+
+public class JWTProviderTest {
+    @Test
+    @DisplayName("엑세스 토큰 기본 테스트")
+    public void testAccessTokenGeneration() {
+        Map<String, Object> claims = Map.of("username", "test-user","id", 1);
+        Date now = new Date();
+        String token = JWTProvider.generateAccessToken(claims, now);
+
+        assertDoesNotThrow(() -> {
+            Map<String, ?> result = JWTProvider.parseClaims(token);
+            assertEquals(claims.get("username"), result.get("username"));
+            assertEquals(claims.get("id"), result.get("id"));
+        });
+    }
+
+    @Test
+    @DisplayName("엑세스 토큰 만료 테스트")
+    public void testAccessTokenExpiration() {
+        Map<String, Object> claims = Map.of("id", 1);
+        Date now = new Date();
+        Date old = new Date(now.getTime() - (JWTProvider.ACCESS_TOKEN_EXPIRATION_INTERVAL + 1));
+        String token = JWTProvider.generateAccessToken(claims, old);
+        assertThrows(InvalidJWTException.class, () -> {
+            JWTProvider.parseClaims(token);
+        });
+    }
+
+    @Test
+    @DisplayName("잘못된 엑세스 토큰 테스트")
+    public void testInvalidAccessToken() {
+        String token = "1231231223123";
+        assertThrows(InvalidJWTException.class, () -> {
+            JWTProvider.parseClaims(token);
+        });
+    }
+}
