e9b98ef7376acf3658e7466cb66c1da101000500
commaengddongyeon <kimdongyn927@naver.com>
2024-12-16 10:39:23 +0900

    :bug: 상품 추천 repo, service 삭제

--- src/main/java/com/saltyhana/saltyhanaserver/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.saltyhana.saltyhanaserver.repository;
-
-import com.saltyhana.saltyhanaserver.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
