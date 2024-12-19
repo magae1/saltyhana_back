4ea607ed8557291b810d0363ce7b9d4220b069b1
commaengddongyeon <kimdongyn927@naver.com>
2024-12-19 14:43:48 +0900

    :bug: SecurityContext 추가

--- src/main/java/com/saltyhana/saltyhanaserver/controller/ProductController.java
+++ src/main/java/com/saltyhana/saltyhanaserver/controller/ProductController.java
@@ -3,23 +3,39 @@ package com.saltyhana.saltyhanaserver.controller;
 import com.saltyhana.saltyhanaserver.dto.RecommendResponseDTO;
 import com.saltyhana.saltyhanaserver.service.RecommendationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("${api_prefix}/products")
+@PreAuthorize("hasAnyRole('ROLE_USER')")
 @RequiredArgsConstructor
-// 상품 추천 페이지 Controller
 public class ProductController {
+
     private final RecommendationService recommendationService;
 
     @PostMapping("/recommend")
-    public ResponseEntity<List<RecommendResponseDTO>> getRecommendations(@RequestParam("UserId") Long userId) {
+    public ResponseEntity<List<RecommendResponseDTO>> getRecommendations() {
+        Long userId = getUserId();
+
+        log.info("Authenticated User ID: {}", userId);
+
         List<RecommendResponseDTO> recommendations = recommendationService.getRecommendations(userId);
         return ResponseEntity.ok(recommendations);
     }
 
+    private Long getUserId() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return (Long) auth.getPrincipal(); // Principal에서 사용자 ID(Long) 반환
+    }
 }
-
