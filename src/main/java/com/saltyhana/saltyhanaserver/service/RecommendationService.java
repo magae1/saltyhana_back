46452f0822bfa997a92d572ca1fd656fb9db7b5e
commaengddongyeon <kimdongyn927@naver.com>
2024-12-19 14:43:48 +0900

    :bug: SecurityContext 추가

--- src/main/java/com/saltyhana/saltyhanaserver/service/RecommendationService.java
+++ src/main/java/com/saltyhana/saltyhanaserver/service/RecommendationService.java
@@ -1,56 +1,3 @@
-//package com.saltyhana.saltyhanaserver.service;
-//
-//import com.saltyhana.saltyhanaserver.dto.RecommendResponseDTO;
-//import com.saltyhana.saltyhanaserver.entity.Product;
-//import com.saltyhana.saltyhanaserver.entity.User;
-//import com.saltyhana.saltyhanaserver.mapper.RecommendationMapper;
-//import com.saltyhana.saltyhanaserver.repository.ProductRepository;
-//import com.saltyhana.saltyhanaserver.repository.UserRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.Optional;
-//import java.util.stream.Collectors;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class RecommendationService {
-//
-//    private final UserRepository userRepository;
-//    private final ProductRepository productRepository;
-//    private final AnthropicService anthropicService;
-//
-//    public List<RecommendResponseDTO> getRecommendations(Long userId) {
-//        Optional<User> optionalUser = userRepository.findById(userId);
-//        if (optionalUser.isEmpty()) {
-//            throw new IllegalArgumentException("해당하는 유저가 존재하지 않습니다.");
-//        }
-//
-//        User user = optionalUser.get();
-//        if (user.getConsumptionTendency() == null) {
-//            // description이 없는 경우 인기 상품 반환
-//            List<Product> popularProducts = productRepository.findAllByIdBetween(1L, 6L);
-//            return RecommendationMapper.toPopularDTOList(popularProducts); // Rate 정보 필요 없음
-//        } else {
-//            // description이 있는 경우 Anthropic API 호출
-//            String description = user.getConsumptionTendency().getDescription();
-//            String birth = user.getBirth().toString();
-//
-//            // Anthropic API 호출 및 상품명 리스트 추출
-//            List<String> productNames = anthropicService.getRecommendations(birth, description);
-//
-//            // 상품명으로 Product 테이블에서 검색
-//            List<Product> matchedProducts = productRepository.findAllByFinPrdtNmIn(productNames);
-//
-//            // DTO 변환
-//            return RecommendationMapper.toTendencyDTOList(matchedProducts, description); // Rate 정보 필요 없음
-//        }
-//    }
-//}
-//
-//
-// 거의 description 제외 완료1
 package com.saltyhana.saltyhanaserver.service;
 
 import com.saltyhana.saltyhanaserver.dto.RecommendResponseDTO;
@@ -123,5 +70,6 @@ public class RecommendationService {
                         rate -> rate.getProduct().getId(),
                         rate -> rate
                 ));
+
     }
 }
