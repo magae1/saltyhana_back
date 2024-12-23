500f003da15e3da5b61e75df64ddf065e1f41011
haneul0509 <haneul0509@naver.com>
2024-12-23 18:13:05 +0900

    :dizzy: Icon 예외처리 수정

--- src/main/java/com/saltyhana/saltyhanaserver/repository/GoalRepository.java
+++ src/main/java/com/saltyhana/saltyhanaserver/repository/GoalRepository.java
@@ -3,6 +3,8 @@ package com.saltyhana.saltyhanaserver.repository;
 import com.saltyhana.saltyhanaserver.entity.Goal;
 import com.saltyhana.saltyhanaserver.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,4 +13,7 @@ public interface GoalRepository extends JpaRepository<Goal, Long> {
     List<Goal> findAllByUser(User user);
     List<Goal> findByUser(User user);
     List<Goal> findByUserAndIsEndedFalse(User user);
+
+    @Query("SELECT g FROM Goal g LEFT JOIN FETCH g.icon WHERE g.user = :user")
+    List<Goal> findAllByUserWithIcons(@Param("user") User user);
 }
